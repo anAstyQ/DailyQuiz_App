292268aaaf0e33f3a71b7e9ec088fc6d40cd13ed
anAstyQ <anabezrukavaya@gmail.com>
2019-02-13 15:31:32 +0200

    Update JS_DQ.js

--- JS_DQ.js
+++ JS_DQ.js
@@ -10,13 +10,9 @@
         jQuery('a.collapse').on('click', function (){
             var elemToShow = jQuery(this).closest('p').next('div');
     
-            console.log(elemToShow);
-            console.log(elemToShow.css('display'));
             if(elemToShow.css('display') == 'none') { 
-                console.log('none');
                 elemToShow.show();
             } else {
-                console.log('block');
                 elemToShow.hide();
             } 
         });
